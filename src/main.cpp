3ac5b456f1d0023e5f1df884d3ba57ed172c59f6
vsrushti18 <vsrushti18@gmail.com>
2025-04-13 22:24:03 +0530

    Milestone 1.2  Implemented deep sleep and RTC wake-up counter

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,40 @@
 #include <Arduino.h>
-#include "SimulatedQMI8658.h"
 
-SimulatedQMI8658 sensor;
+RTC_DATA_ATTR int wakeCounter = 0; 
 
-IMUdata acc[1];
-IMUdata gyr[1];
-
-void onMotionDetected() {
-    Serial.println(">> Motion callback triggered!");
-    sensor.readFromFifo(acc, 1, gyr, 1);
-
-    Serial.print("Accel: ");
-    Serial.print(acc[0].x); Serial.print(", ");
-    Serial.print(acc[0].y); Serial.print(", ");
-    Serial.println(acc[0].z);
-
-    Serial.print("Gyro: ");
-    Serial.print(gyr[0].x); Serial.print(", ");
-    Serial.print(gyr[0].y); Serial.print(", ");
-    Serial.println(gyr[0].z);
+void goToSleep() {
+    Serial.println("[Sleep] Preparing to sleep for 2 minutes...");
+    esp_sleep_enable_timer_wakeup(2 * 60 * 1000000); 
+    Serial.println("[Sleep] Going to deep sleep now. See you later 💤");
+    delay(100); 
+    esp_deep_sleep_start();
 }
 
 void setup() {
     Serial.begin(115200);
-    delay(1000); // Let Serial wake up
-    sensor.begin();
-    sensor.configWakeOnMotion();
-    sensor.setWakeupMotionEventCallBack(onMotionDetected);
+    delay(1000); 
+
+    wakeCounter++;
+    Serial.println("==== Wake-up Event Detected ====");
+    Serial.print("[Wake] Wake counter: ");
+    Serial.println(wakeCounter);
+
+    esp_sleep_wakeup_cause_t reason = esp_sleep_get_wakeup_cause();
+    Serial.print("[Wake] Wake-up reason: ");
+    switch (reason) {
+        case ESP_SLEEP_WAKEUP_TIMER:
+            Serial.println("Timer (2-minute timeout)");
+            break;
+        case ESP_SLEEP_WAKEUP_UNDEFINED:
+            Serial.println("Power-on or reset");
+            break;
+        default:
+            Serial.println("Other wake-up source");
+    }
 }
 
 void loop() {
-    sensor.simulateMotion(); // randomly simulates motion
-    delay(500);
+    Serial.println("[Loop] Running... will sleep in 5 seconds");
+    delay(5000); 
+    goToSleep();
 }
